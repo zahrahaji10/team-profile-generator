5267c36c15b47727fd72d0679b526a254a5451f5
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-06-14 16:50:04 +0100

    main questions and  manager questions array

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
+// import inquirer into this application
+const inquirer = require("inquirer");
 
+// declare questions objects array using inquirer
+
+// first question user will be asked
+const startQuestions = [
+  {
+    message: "Please enter team name",
+    type: "input",
+    name: "teamName",
+    // validate that user entered a team name
+    validate: (enteredTeamName) => {
+      if (enteredTeamName) {
+        return true;
+      } else {
+        return "Please enter a title for your team to continue";
+      }
+    },
+  },
+  {
+    message: "Select the type of employee to add to the team",
+    type: "list",
+    name: "typeOfEmployee",
+    choice: ["Manager", "Engineer", "Intern"],
+  },
+];
