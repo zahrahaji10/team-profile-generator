4a989fd54208b9f4f3745dc2d150a1d8b4ba304e
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-06-14 17:03:30 +0100

    questions array done

--- src/index.js
+++ src/index.js
@@ -25,3 +25,109 @@ const startQuestions = [
     choice: ["Manager", "Engineer", "Intern"],
   },
 ];
+
+// manager questions array
+const managerQuestions = [
+  {
+    message: "Please enter manager name",
+    type: "input",
+    name: "name",
+    validate: (enteredName) => {
+      if (enteredName) {
+        return true;
+      } else {
+        return "Please enter manager name to continue";
+      }
+    },
+  },
+  {
+    message: "Please enter manager ID",
+    type: "input",
+    name: "id",
+    validate: (enteredId) => {
+      if (enteredId) {
+        return true;
+      } else {
+        return "Please enter manager name to continue";
+      }
+    },
+  },
+  {
+    message: "Please enter manager email address ",
+    type: "input",
+    name: "email",
+    validate: (enteredEmail) => {
+      if (enteredEmail) {
+        return true;
+      } else {
+        return "Please enter manager email address to continue";
+      }
+    },
+  },
+  {
+    message: "Please enter manager office number",
+    type: "input",
+    name: "officeNumber",
+    validate: (enteredOfficeNumber) => {
+      if (enteredOfficeNumber) {
+        return true;
+      } else {
+        return "Please enter manager office number to continue";
+      }
+    },
+  },
+];
+
+// manager questions array
+
+// manager questions array
+const engineerQuestions = [
+  {
+    message: "Please enter engineer name",
+    type: "input",
+    name: "name",
+    validate: (enteredName) => {
+      if (enteredName) {
+        return true;
+      } else {
+        return "Please enter name to continue";
+      }
+    },
+  },
+  {
+    message: "Please enter ID number",
+    type: "input",
+    name: "id",
+    validate: (enteredId) => {
+      if (enteredId) {
+        return true;
+      } else {
+        return "Please enter ID number to continue";
+      }
+    },
+  },
+  {
+    message: "Please enter email address ",
+    type: "input",
+    name: "email",
+    validate: (enteredEmail) => {
+      if (enteredEmail) {
+        return true;
+      } else {
+        return "Please enter email address to continue";
+      }
+    },
+  },
+  {
+    message: "Please enter github username",
+    type: "input",
+    name: "github",
+    validate: (enteredGithub) => {
+      if (enteredGithub) {
+        return true;
+      } else {
+        return "Please enter github username to continue";
+      }
+    },
+  },
+];
