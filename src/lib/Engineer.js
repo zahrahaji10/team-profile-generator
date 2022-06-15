c10eab0aac65ad4bf04dbb6cd1faf040febb1d2d
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-06-15 13:10:12 +0100

    created engineer class

---
+++ src/lib/Engineer.js
@@ -0,0 +1,17 @@
+// import Employee.js into file
+const Employee = require("./Employee");
+
+// engineer class extending from main parent class
+class Engineer extends Employee {
+  constructor(name, id, email, github) {
+    super(name, id, email);
+    this.github = github;
+    this.role = "Engineer";
+  }
+
+  getGithub() {
+    return this.github;
+  }
+}
+
+module.exports = Engineer;
